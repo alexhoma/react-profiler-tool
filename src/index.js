477a15baa9c7b06fb8edb4838ff68a9bbbb77ee1
Alex <acasajuana@trovit.com>
2018-11-28 18:37:32 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = () =>{
-  return (
-    <div>
-      <h1>Hello Parcel</h1>
-    </div>
-  )
+const SearchBox = onChange => {
+  return <input type="text" onChange={onChange} />;
 };
 
-render(<App />, document.getElementById("app"));
+const Result = hits => {
+  if (!hits) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {hits.map(hit => (
+        <li>{hit}</li>
+      ))}
+    </ul>
+  );
+};
+
+class MiniTrovit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: null
+    };
+  }
+
+  onUserSearch = e => {
+    console.log("searching");
+  };
+
+  render() {
+    return (
+      <section>
+        <h1>MiniTrovit</h1>
+        <SearchBox onChange={this.onUserSearch} />
+        <Result hits={this.state.hits} />
+      </section>
+    );
+  }
+}
+
+render(<MiniTrovit />, document.getElementById("app"));
