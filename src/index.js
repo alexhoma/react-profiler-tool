52a155cabe916916a79e9e232a4a9b306572cf7b
alexhoma <alexcm.14@gmail.com>
2018-12-08 22:09:29 +0100

    feat: testing with a more complex case with many prop changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
-import { search } from "./search";
+import { search, highlight } from "./lib";
 
-function SearchBox({ onChange }) {
+function SearchBox({ onChange, type }) {
   return (
     <input
-      type="text"
-      placeholder="Search something..."
+      type="search"
+      placeholder="Type to search..."
       onChange={e => onChange(e)}
     />
   );
 }
 
-function Item({ item }) {
-  return <li>{item}</li>;
+function HighlightedTitle({ query, title }) {
+  const highlighted = highlight(query, title);
+  return (
+    <span
+      dangerouslySetInnerHTML={{
+        __html: highlighted
+      }}
+    />
+  );
 }
 
-function Result({ hits }) {
+function Price({ price }) {
+  return <span>{price}</span>;
+}
+
+function Item({ children }) {
+  return <li>{children}</li>;
+}
+
+function Result({ query, hits }) {
   return (
     <ul>
       {hits.map((item, key) => (
-        <Item key={`${key}-hit`} item={item} />
+        <Item key={item.id}>
+          <HighlightedTitle query={query} title={item.title} />
+          <Price price={item.price} />
+        </Item>
       ))}
     </ul>
   );
@@ -28,20 +46,23 @@ function Result({ hits }) {
 
 class MiniTrovit extends React.Component {
   state = {
+    query: "",
     hits: []
   };
 
   onUserSearch = e => {
-    const hits = search(e.target.value);
-    this.setState({ hits });
+    const query = e.target.value;
+    const hits = search(query);
+    this.setState({ query, hits });
   };
 
   render() {
     return (
       <section>
         <h1>🏠 MiniTrovit</h1>
+        <p>A lite version to search for your next life-changing home!</p>
         <SearchBox onChange={this.onUserSearch} />
-        <Result hits={this.state.hits} />
+        <Result query={this.state.query} hits={this.state.hits} />
       </section>
     );
   }
---
+++ src/lib.js
@@ -0,0 +1,23 @@
+import haystack from "./data.json";
+
+export function search(needle) {
+  if (needle === "") {
+    return [];
+  }
+
+  return haystack.filter(
+    item =>
+      item.title
+        .toLowerCase()
+        .replace(", ", " ")
+        .match(needle.toLowerCase()) && item
+  );
+}
+
+export function highlight(toHighlight, fullText) {
+  let regex = new RegExp(`(${toHighlight})`, "gi");
+  let highlighted = fullText.replace(regex, "<em>$1</em>");
+  let sanitizedSpaces = highlighted.split(" ");
+
+  return sanitizedSpaces.join("&nbsp;");
+}
