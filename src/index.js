30510d3645b8d88b801319879b75ee2f77fcb612
alexhoma <alexcm.14@gmail.com>
2018-12-09 00:27:25 +0100

    fix: remove type prop

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { search, highlight } from "./lib";
 
-function SearchBox({ onChange, type }) {
+function SearchBox({ onChange }) {
   return (
     <input
       type="search"
